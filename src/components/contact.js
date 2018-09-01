91e46c2053ab28f07c47d35fed914b21c4881e01
Hannah Lee <hanlavlee@gmail.com>
2018-08-31 23:19:28 -0700

    Add content and style Contact page.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,64 @@
 import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 
 class Contact extends Component {
-  state = {};
   render() {
     return (
-      <div>
-        <h1>Contact Page</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Hannah Lee</h2>
+            <img
+              src="https://thumbs.dreamstime.com/b/asian-chinese-woman-flat-vector-design-style-can-be-used-profile-avatar-illustration-79782777.jpg"
+              alt="avatar"
+              style={{ height: "250px" }}
+            />
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "26px", fontFamily: "Helvetica" }}
+                  >
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (123) 456-7890
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "26px", fontFamily: "Helvetica" }}
+                  >
+                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                    https://linkedin.com
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "26px", fontFamily: "Helvetica" }}
+                  >
+                    <i className="fa fa-envelope-square" aria-hidden="true" />
+                    yourname@gmail.com
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
