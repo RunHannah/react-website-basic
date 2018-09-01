e20fd0582f5583ecec921e403280577dc35df53d
Hannah Lee <hanlavlee@gmail.com>
2018-08-31 21:29:40 -0700

    Add cards for projects page.

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,105 @@
 import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  CardText,
+  Button,
+  CardMenu,
+  IconButton
+} from "react-mdl";
 
 class Projects extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#000000",
+              height: "176px",
+              background:
+                "url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1000px-React-icon.svg.png) center / cover"
+            }}
+          >
+            Portfolio website
+          </CardTitle>
+          <CardText>ReactJS with React Router</CardText>
+          <CardActions border>
+            <Button colored>GitHub</Button>
+            <Button colored>Live Demo</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#000000" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is NodeJS</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is Ruby on Rails</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#000000",
+              height: "176px",
+              background:
+                "url(https://angular.io/assets/images/logos/angular/angular.png) center / cover"
+            }}
+          >
+            National Park Service Clone
+          </CardTitle>
+          <CardText>Built with Angular and Firebase</CardText>
+          <CardActions border>
+            <Button colored>GitHub</Button>
+            <Button colored>Live Demo</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#000000" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Projects Page</h1>
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>NodeJS</Tab>
+          <Tab>Ruby on Rails</Tab>
+          <Tab>Angular</Tab>
+        </Tabs>
+
+        <Grid className="projects-grid">
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
